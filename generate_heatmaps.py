52cb41396f419e75e79f9f0aca76ac7ccb3b2256
Arunesh Mishra <41530378+aruneshmee@users.noreply.github.com>
2020-02-20 16:40:57 +0800

    Update generate_heatmaps.py

--- generate_heatmaps.py
+++ generate_heatmaps.py
@@ -1,13 +1,18 @@
+# Importing the lib
 import pandas as pd
 import seaborn as sb
 import matplotlib.pyplot as plt
 
+# Importing dataset
 data = pd.read_csv('comb_data.csv')
 
+# Using Pearson to find pairwise relation across all the columns
 pearsoncorr = data.corr(method='pearson')
 
+# Magnifying image 
 plt.subplots(figsize=(40,35))
 
+# Generating heatmap using seaborn
 svm = sb.heatmap(pearsoncorr, 
             xticklabels=pearsoncorr.columns,
             yticklabels=pearsoncorr.columns,
@@ -15,5 +20,6 @@ svm = sb.heatmap(pearsoncorr,
             annot=True,
             linewidth=0.8)
 
+# Saving image
 figure = svm.get_figure()    
 figure.savefig('heatmap_hydrolic_press.png', dpi=400)
