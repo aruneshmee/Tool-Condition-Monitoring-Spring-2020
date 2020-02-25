c8b11b0f1277a8c3e1b3f7d0aecad9d266bbc890
Arunesh Mishra <41530378+aruneshmee@users.noreply.github.com>
2020-02-25 12:44:14 +0800

    Update fault_analyser.py

--- fault_analyser.py
+++ fault_analyser.py
@@ -12,3 +12,24 @@ data = pd.read_csv('comb_data.csv')
 df = pd.DataFrame(data)
 num_col=len(df.axes[1])
 num_row=len(df.axes[0])
+
+# Section 1: Generating heat maps of  the correlation between each factore
+def generating_heatmap():
+
+    # Using Pearson to find pairwise relation across all the columns
+    pearsoncorr = data.corr(method='pearson')
+
+    # Magnifying image 
+    plt.subplots(figsize=(40,35))
+
+    # Generating heatmap using seaborn
+    svm = sb.heatmap(pearsoncorr, 
+                xticklabels=pearsoncorr.columns,
+                yticklabels=pearsoncorr.columns,
+                cmap='RdBu_r',
+                annot=True,
+                linewidth=0.8)
+
+    # Saving image
+    figure = svm.get_figure()    
+    return figure.savefig('heatmap_hydrolic_press.png', dpi=400)
